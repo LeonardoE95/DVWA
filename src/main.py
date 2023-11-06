e449ebc3ad8f361736cc4e652c0bae9a01c98280
leo123 <leo95.yt@gmail.com>
2023-11-06 19:05:42 +0100

    Added CSRF section

--- src/main.py
+++ src/main.py
@@ -2,21 +2,21 @@
 
 from util import init_app
 
-# from sqli.low import sqli_low
-# from sqli.medium import sqli_medium
-# from sqli.high import sqli_high
+from sqli.low import sqli_low
+from sqli.medium import sqli_medium
+from sqli.high import sqli_high
 
-# from sqli_blind.low import sqli_blind_low
-# from sqli_blind.medium import sqli_blind_medium
-# from sqli_blind.high import sqli_blind_high
+from sqli_blind.low import sqli_blind_low
+from sqli_blind.medium import sqli_blind_medium
+from sqli_blind.high import sqli_blind_high
 
-# from bruteforce.low import bruteforce_low
-# from bruteforce.medium import bruteforce_medium
-# from bruteforce.high import bruteforce_high
+from bruteforce.low import bruteforce_low
+from bruteforce.medium import bruteforce_medium
+from bruteforce.high import bruteforce_high
 
-# from command_injection.low import command_injection_low
-# from command_injection.medium import command_injection_medium
-# from command_injection.high import command_injection_high
+from command_injection.low import command_injection_low
+from command_injection.medium import command_injection_medium
+from command_injection.high import command_injection_high
 
 # --------------------------------
 
